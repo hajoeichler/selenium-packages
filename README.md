9ddc3da3fc87fe208852a8b732ccaf3097ddc250
Jens Braeuer <jens.braeuer@commercetools.de>
2012-03-15 14:10:19 +0000

    Remove Rubygem packaging. Moved to dedicated repo.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-This repo holds script to package Selenium (standalone-server and Ruby
-Gems) as Debian packages.
+This repo holds script to package Selenium (standalone-server) as Debian packages.
 
 Tested on Ubuntu 11.10.
 
--- package.sh
+++ package.sh
@@ -28,14 +28,6 @@ package_jar() {
     fpm -s dir -t deb --name "$name" --version "$version" -a all -C "$work/$version" .
 }
 
-package_gem() {
-    log "Package GEM $name - $version"
-    cd "$work"
-    mkdir "$name"
-    gem1.9.1 install --no-ri --no-rdoc --install-dir "$work/$name" $name
-    find "$work/$name" -name '*.gem' | xargs -rn1 fpm -s gem -t deb --gem-gem /usr/bin/gem1.9.1 --gem-package-prefix=rubygem19 --depends "rubygems1.9.1"
-}
-
 base=$(dirname $(readlink -f "$0"))
 work="$base/work"
 download="$base/download"
@@ -47,11 +39,3 @@ name=selenium-server-standalone
 version=2.20.0
 get
 package_jar
-
-cd "$base"
-cat testing-gems.txt | while read line; do
-name=$(echo "$line" | awk '{ print $1 }')
-    version=$(echo "$line" | awk '{ print $2 }')
-    echo "Will do $name at $version"
-    package_gem
-done
